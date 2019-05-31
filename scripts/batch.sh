49c57dc9142c0672eea25e3bc19f7767351fddda
nakamura196 <na.kamura.1263@gmail.com>
2019-05-31 20:29:53 +0900

    test

--- scripts/batch.sh
+++ scripts/batch.sh
@@ -4,13 +4,13 @@ python get_color_data.py ../data/captures.json ../img/items/ ../data/item_hsl.js
 python get_colors.py ../data/item_hsl.json ../data/colors.json ../data/item_colors.json;
 python get_dates.py ../data/src/pd_items.json ../data/centuries.json ../data/item_centuries.json century;
 python get_genres.py ../data/src/pd_items.json ../data/genres.json ../data/item_genres.json;
-python stitch_images.py ../data/ ../img/items/ ../img/ 100 10 10 default 50 20 3 30000
-python stitch_images.py ../data/ ../img/items/ ../img/ 100 10 10 centuries 50 20 3 30000
-python stitch_images.py ../data/ ../img/items/ ../img/ 100 10 10 collections 50 20 3 30000
-python stitch_images.py ../data/ ../img/items/ ../img/ 100 10 10 colors 50 20 3 30000
-python stitch_images.py ../data/ ../img/items/ ../img/ 100 10 10 genres 50 20 3 30000
-python stitch_images.py ../data/ ../img/items/ ../img/ 100 80 80 colors 50 20 3 8000
+python stitch_images.py ../data/ ../img/items/ ../img/ 50 20 20 default 50 20 3 30000
+python stitch_images.py ../data/ ../img/items/ ../img/ 50 20 20 centuries 50 20 3 30000
+python stitch_images.py ../data/ ../img/items/ ../img/ 50 20 20 collections 50 20 3 30000
+python stitch_images.py ../data/ ../img/items/ ../img/ 50 20 20 colors 50 20 3 30000
+python stitch_images.py ../data/ ../img/items/ ../img/ 50 20 20 genres 50 20 3 30000
 python generate_metadata.py ../data/src/pd_items.json ../js/items/ 5;
-python generate_labels.py ../data/ ../js/labels.json 100 10 50 20 3;
-python generate_coordinates.py ../data/ ../js/coords.json 100 10 10 50 20 3;
+python generate_labels.py ../data/ ../js/labels.json 50 20 50 20 3;
+python generate_coordinates.py ../data/ ../js/coords.json 50 20 20 50 20 3;
+
 
